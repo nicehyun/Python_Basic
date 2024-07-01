ee56583535a71894402d9b6926ee5765c81a444e
ish <lshsh5656@naver.com>
2024-07-01 17:36:18 +0900

    Refactor : 예외처리

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ import pandas
 
 nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-# TODO 1. Create a dictionary in this format:
 nato_dict = {row.letter: row.code for index, row in nato_df.iterrows()}
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ").upper()
 
-output_list = [nato_dict[letter] for letter in word]
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
 
-print(output_list)
+
+generate_phonetic()
